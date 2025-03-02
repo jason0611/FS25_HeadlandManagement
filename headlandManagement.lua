fdf58a50fd10f6fd247c4e99624de25010234452
Glowins Modschmiede <martin@eller-essen.de>
2025-03-02 10:13:24 +0100

    Update headlandManagement.lua

--- headlandManagement.lua
+++ headlandManagement.lua
@@ -781,7 +781,6 @@ function HeadlandManagement:onPostLoad(savegame)
 	self.configurations["HeadlandManagement"] = spec.exists and 2 or 1
 	dbgprint("onPostLoad : HLM exists (finally): "..tostring(spec.exists))
 	dbgprint_r(self.configurations, 2, 0)
-	self:raiseDirtyFlags(spec.dirtyFlag)
 end
 
 function HeadlandManagement:saveToXMLFile(xmlFile, key, usedModNames)
