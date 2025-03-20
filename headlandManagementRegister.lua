97dbb72d005dc49acf0560f178b6ea260b1c26ba
Glowins Modschmiede <martin@eller-essen.de>
2025-03-20 12:33:20 +0100

    3.0.0.2
    Fix for swapping implements with some mod vehicles, caused by arbitrary entry-sequences in tables

--- headlandManagementRegister.lua
+++ headlandManagementRegister.lua
@@ -1,15 +1,22 @@
 --
--- Register Headland Management for LS 22
+-- Register Headland Management for LS 25
 --
 -- Jason06 / Glowins Modschmiede 
--- Version 2.2.0.0
+-- Version 3.0.0.0
 --
 
 local specName = g_currentModName..".HeadlandManagement"
 
+-- add specialization
 if g_specializationManager:getSpecializationByName("HeadlandManagement") == nil then
   	g_specializationManager:addSpecialization("HeadlandManagement", "HeadlandManagement", g_currentModDirectory.."headlandManagement.lua", nil)
   	dbgprint("Specialization 'HeadlandManagement' added", 2)
+end
+
+-- add configuration
+if g_vehicleConfigurationManager.configurations["headlandManagement"] == nil then		
+	g_vehicleConfigurationManager:addConfigurationType("headlandManagement", g_i18n.modEnvironments[HeadlandManagement.MOD_NAME]:getText("text_HLM_configuration"), "headlandManagement", VehicleConfigurationItem, nil, nil, nil, 2000)
+	dbgprint("Configuration 'HeadlandManagement' added", 2)
 end
 
 for typeName, typeEntry in pairs(g_vehicleTypeManager.types) do
